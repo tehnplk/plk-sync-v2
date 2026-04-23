26b7cdce7faf68f015b9b3aa6616e44480bb0f2a
tehnplk <tehnplk@gmail.com>
2026-04-23 15:58:39 +0700

    Limit visit type query through current date

--- postgres_visit_type_count.sql
+++ postgres_visit_type_count.sql
@@ -9,6 +9,6 @@ SELECT
     SUM(CASE WHEN i.export_code = '5' THEN 1 ELSE 0 END) AS visit_type_5
 FROM ovst o
 LEFT JOIN ovstist i ON o.ovstist = i.ovstist
-WHERE o.vstdate >= DATE '2026-03-23'
+WHERE o.vstdate::date BETWEEN DATE '2026-03-23' AND CURRENT_DATE
 GROUP BY COALESCE(NULLIF(o.hcode, ''), (SELECT hospitalcode FROM opdconfig LIMIT 1)), o.vstdate::date
 ORDER BY COALESCE(NULLIF(o.hcode, ''), (SELECT hospitalcode FROM opdconfig LIMIT 1)), o.vstdate::date;
